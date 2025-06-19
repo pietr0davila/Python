9b090c1826d718cae4f316de567367673931e976
pietr0davila <dsescuderop@gmail.com>
2025-06-19 11:42:41 -0300

    Arg parser

---
+++ Crack_My_Hash/get_args.py
@@ -0,0 +1,14 @@
+import argparse
+
+def create_args():
+    parser = argparse.ArgumentParser (
+        prog="\n===== Crack My Hash =====\n",
+        description="Use main.py --help command to see all the options",
+        epilog="Project made for educational purposes\n",
+    )
+
+    parser.add_argument("hash", help="Hash to be cracked, first positional argument")
+    parser.add_argument("wordlist", help="wordlist that will be used to crack the hash, second positional arguments")
+    parser.add_argument("-ht", "--hash_type", help="hash Type, use --hash_type_help to get all the options")
+    args = parser.parse_args()
+    
---
+++ Crack_My_Hash/main.py
@@ -0,0 +1,12 @@
+from get_args import create_args
+# Script que quebra senhas usando wordlists
+# 1. Precisa de leitura de todas as palavras da wordlist
+# 2. precisa transformar o texto da wordlist no hash correspondente
+# 3. Compara o hash da palavra com o hash fornecido pelo usuário
+
+# Funções: 
+# 1. suporte a vários tipos de hash (Hashlib)
+# 2. Argumentos via Cli - --hash, --wordlist, --type...
+
+if __name__ == "__main__":
+    create_args()
